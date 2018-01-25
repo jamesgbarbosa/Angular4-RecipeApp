5636147bde2a13985eeac1e47ea7530af516b92c
jambarbo <james.barbosa@nokia.com>
2018-01-25 16:39:47 +0800

    Restructured Services to use child injectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,11 @@ import {FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import { ErrorPageComponent } from './error-page/error-page.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import {RecipeService} from './recipes/recipe.service';
-import {DataStorageService} from './shared/data-storage.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth.guard';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
-import { HomeComponent } from './core/home/home.component';
 import {CoreModule} from "./core/core.module";
 
 
@@ -35,7 +28,7 @@ import {CoreModule} from "./core/core.module";
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,17 @@ import {HomeComponent} from "./home/home.component";
 import {HeaderComponent} from "./header/header.component";
 import {SharedModule} from "../shared/shared.module";
 import {AppRoutingModule} from "../app-routing.module";
-import {CommonModule} from "@angular/common";
+import {AuthGuard} from "../auth/auth.guard";
+import {AuthService} from "../auth/auth.service";
+import {DataStorageService} from "../shared/data-storage.service";
+import {RecipeService} from "../recipes/recipe.service";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
 @NgModule({
   declarations: [HomeComponent, HeaderComponent],
   imports: [SharedModule, AppRoutingModule],
-  exports: [HeaderComponent, AppRoutingModule]
+  exports: [HeaderComponent, AppRoutingModule],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard]
 })
 export class CoreModule {
 
